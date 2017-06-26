52ecb59fa195570440bcbc052d129cefb92e6eb1
su8hash <su8hash@gmail.com>
2017-06-26 22:01:31 +0530

    Adding Instructions

--- UserPrompt.js
+++ UserPrompt.js
@@ -13,7 +13,13 @@ export default class UserPrompt extends Component{
         return(
             <View>
                 <Text>
-                    User Promt
+                   Please Read This instuction These are different.
+                   There Will be Total 14 Question
+                   First Question will Give you 50 points and points will double on every question
+                   There are  three lifeline 
+                   1) 50 - 50 (remove two wrong answers)
+                   2) Double Try (You can try two answers)
+                   3) Flip (You can change question before answering it)
                 </Text>
                  <Button title = "Ok" onPress = {()=>this.props.instuctionDone()}  />
             </View>
