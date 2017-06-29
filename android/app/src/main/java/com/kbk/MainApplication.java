d1b95602ce57af4a206f7df783d39b689cc792f9
su8hash <su8hash@gmail.com>
2017-06-30 00:05:10 +0530

    add running

--- android/app/src/main/java/com/kbk/MainApplication.java
+++ android/app/src/main/java/com/kbk/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.rctrevmob.RevMobPackage;
+import com.rctrevmob.RevMobPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,8 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RevMobPackage(),
-          new RevMobPackage()
+            new RevMobPackage()
       );
     }
   };
