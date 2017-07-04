6cdc0dd4a6e0aa94dc6bc4c9a17b7c95cab67d2e
su8hash <su8hash@gmail.com>
2017-07-05 00:12:00 +0530

    loose freeze bug fixed

--- Loose.js
+++ Loose.js
@@ -12,31 +12,45 @@ import { NativeAppEventEmitter } from 'react-native';
 
 export  default class Loose extends Component{
 
+ constructor(){
+     super();
+     this.listners = [];
+     this.state = {fetchingAdd:true}
+ }
+
+ 
  componentWillMount () {
-        // NativeAppEventEmitter.addListener(
-        //     'onRevmobRewardedVideoDidLoad',
-        //         (e)=>{ RevMobManager.showRewardedVideo(); }
-        // );
         NativeAppEventEmitter.addListener(
+            'onRevmobRewardedVideoDidLoad',
+                (e)=>{ this.setState({fetchingAdd:false})}
+        );
+  this.listners.push(NativeAppEventEmitter.addListener(
             'onRevmobRewardedVideoComplete',
                 (e)=>{
                     console.warn("add was completed");
                     this.props.retry();
                 }
-        );
+        ));
     }
     componentDidMount () {
         // move to constructor or better in game screen so that video is already loaded
         RevMobManager.startSession("5942503155bc0f38cb700ae7", function revMobStartSessionCb(err){
             if(!err) RevMobManager.loadRewardedVideo(); // Load rewarded video if session starts successfully.
-        })
+        });
     }
     componentWillUnmount () {
-        NativeAppEventEmitter.removeAllListeners()
+        // NativeAppEventEmitter.removeAllListeners()
+        this.listners.forEach((x)=>x.remove())
     }
 
    showRewardedVideo(){
         RevMobManager.showRewardedVideo(); 
+   }
+
+   getRetryWithAdd(){
+       if(this.state.fetchingAdd)
+       return <Text>Getting Life</Text>
+       else return <Button title = "Watch Video to get another chance" onPress = {()=> this.showRewardedVideo()} style={styles.btn} />
    }
 
     render(){
@@ -46,7 +60,7 @@ export  default class Loose extends Component{
                 <Text style={styles.bigText}>
                     you have LOSS .Play Again to increase score
                 </Text>
-                 <Button title = "Watch Video to get another chance" onPress = {()=> this.showRewardedVideo()} style={styles.btn} />
+                {this.getRetryWithAdd()}
                  <Button title = "Go to Home" onPress = {()=>this.props.navigateTo('Home')} style={styles.btn} />
                  <Button title = "Play Again" onPress = {()=>this.props.navigateTo('Game')} style={styles.btn} />
             </View>
